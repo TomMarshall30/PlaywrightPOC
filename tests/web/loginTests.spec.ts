6affd9ee003e455d6bafa0166d187bbee96f5463
TomMarshall30 <tmarshall@protonmail.com>
2024-10-05 17:04:04 -0300

    Removed valid test

--- tests/web/loginTests.spec.ts
+++ tests/web/loginTests.spec.ts
@@ -1,27 +1,5 @@
 import test from "./hooks";
 
-test('Incorrect Username', async ({loginTestActions, helperActions}) => {
-    await test.step('Fill in credentials and select login', async () => {
-        await loginTestActions.updateUserName('WrongUserName');
-        await loginTestActions.selectLoginButton();
-    })
-    await test.step('Validate error message', async () => {
-        await loginTestActions.validateInvalidLogin();
-        await helperActions.validateScreenshot();
-    })
-});
-
-test('Valid Login', async ({loginTestActions, helperActions, testUserName, testUserPassword}) => {
-    await test.step('Fill in credentials and select login', async () => {
-        await loginTestActions.updateUserName(testUserName);
-        await loginTestActions.updateUserPassword(testUserPassword);
-        await loginTestActions.selectLoginButton();
-    })
-    await test.step('Validate new page', async () => {
-        await helperActions.validateScreenshot();
-    })
-});
-
 [
     { username: 'standard_user' },
     { username: 'locked_out_user' },
@@ -42,5 +20,14 @@ test('Valid Login', async ({loginTestActions, helperActions, testUserName, testU
 
 })
 
-
+test('Login no password', async ({loginTestActions, helperActions}) => {
+    await test.step('Fill in credentials and select login', async () => {
+        await loginTestActions.updateUserName('WrongUserName');
+        await loginTestActions.selectLoginButton();
+    })
+    await test.step('Validate error message', async () => {
+        await loginTestActions.validateInvalidLogin();
+        await helperActions.validateScreenshot();
+    })
+});
 
